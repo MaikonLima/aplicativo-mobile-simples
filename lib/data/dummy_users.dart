7f377c14bd7dd70a7934b9a25b75458b7f31f82e
Maikon_Anderson <maikon.anderson@m3rd.com.br>
2020-07-15 10:42:55 -0400

    Crud Complete

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,34 @@
+import 'package:example_app/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maikon', 
+    email: 'anderson@gmail.com', 
+    avatarUrl: 'https://cdn.pixabay.com/photo/2019/09/25/15/58/ostrich-4504017_960_720.jpg'
+    ),
+    '2': const User(
+    id: '2',
+    name: 'Maria', 
+    email: 'maria@gmail.com', 
+    //avatarUrl: 'https://pixabay.com/pt/photos/mulher-adulto-dentro-pessoas-3083385/'
+    ),
+    '3': const User(
+    id: '3',
+    name: 'Marinho', 
+    email: 'marinho@gmail.com', 
+    //avatarUrl: 'https://pixabay.com/pt/vectors/usuário-homem-perfil-masculino-33638/'
+    ),
+    '4': const User(
+    id: '4',
+    name: 'Marcela', 
+    email: 'marcela@gmail.com', 
+    //avatarUrl: 'https://pixabay.com/pt/photos/ovinos-texel-holanda-texelschaf-4772994/'
+    ),
+    '5': const User(
+    id: '5',
+    name: 'Marcos', 
+    email: 'marcos@gmail.com', 
+    //avatarUrl: 'https://pixabay.com/pt/photos/ovinos-texel-holanda-texelschaf-4772994/'
+    )
+};
---
+++ lib/views/user_list.dart
@@ -0,0 +1,33 @@
+import 'package:example_app/components/user_tile.dart';
+import 'package:example_app/provider/users.dart';
+import 'package:example_app/routes/app_routes.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class UserList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Users users = Provider.of(context);
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Usuárioss'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add), 
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                AppRoutes.USER_FORM
+              );
+            }
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: users.count,
+        itemBuilder: (ctx, i) => UserTile(users.byIndex(i))
+        ),
+    );
+  }
+}
